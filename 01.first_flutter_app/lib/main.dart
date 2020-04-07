edc9a451541930286447adac148fa90b409be38f
Lintang Prayogo <linrangprayogo12@gmail.com>
2020-04-08 06:28:25 +0700

    first commit

---
+++ 01.first_flutter_app/lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title:Text("My First Flutter")),
+        body:  Center(child: Text("My First Flutter App")),
+      ),
+    );
+  }
+}
