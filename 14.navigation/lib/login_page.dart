976d33d9e3fbf879e5e8914abb30b8c554e32b78
Lintang Prayogo <linrangprayogo12@gmail.com>
2020-04-16 14:46:03 +0700

    navigation

---
+++ 14.navigation/lib/login_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:navigation/main_page.dart';
+
+class LoginPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: RaisedButton(child: Text("LOGIN"), onPressed: () {
+            Navigator.pushReplacement(context, MaterialPageRoute(builder:(context){
+              return MainPage();
+            }));
+          }),
+        ),
+      ),
+    );
+  }
+}
---
+++ 14.navigation/lib/second_page.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class SecondPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+         appBar: AppBar(
+          title: Text("Second Page"),
+        ),
+        body: Center(
+          child: RaisedButton(
+              child: Text("Back"),
+              onPressed: () {
+                Navigator.pop(context);
+              }),
+        ),
+      ),
+    );
+  }
+}
